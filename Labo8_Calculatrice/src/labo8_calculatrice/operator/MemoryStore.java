83bb97024dfd86e9bcbd4b00d86abfada3e77ac9
Unknown <james.smith@heig-vd.ch>
2017-11-22 13:58:35 +0100

    Creation of the operators classes

---
+++ Labo8_Calculatrice/src/labo8_calculatrice/operator/MemoryStore.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labo8_calculatrice.operator;
+
+/**
+ *
+ * @author James
+ */
+public class MemoryStore {
+    
+}
